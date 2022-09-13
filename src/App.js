feea457a9f43d2838494761915bf6c1165e131d0
REVSSPACE <68291519+REVSSPACE@users.noreply.github.com>
2022-09-13 16:50:43 +0530

    sidepart

--- src/App.js
+++ src/App.js
@@ -21,11 +21,46 @@ class App extends Component {
   //  const toptab3 = () => {
   //     ReactDOM.render(<Toptab3 />,document.getElementById("display"));
   //  }
+     const lay =()=> {
+      const x= document.getElementById("laysub");
+      x.style.display ="block";
+     }
+     const build =()=> {
+      const x= document.getElementById("buildsub");
+      x.style.display ="block";
+     }
+
+     const Reclas =()=> {
+      const x= document.getElementById("reclasssub");
+      x.style.display ="block";
+     }
+
 
     return (
       <div id="outer">
           {/* <Coverpage /> */}
-
+          <div id="list">
+            <ul>
+              <li onClick={lay}>Layout Approval
+                  <ul  id="laysub" hidden>
+                    <li>Layout Application</li>
+                    <li>Layout Approval</li>
+                  </ul>
+              </li> 
+              <li onClick={build}>Building Plan Permission
+              <ul id="buildsub" hidden>
+                    <li>Building Plan Application</li>
+                    <li>Building Plan Approval</li>
+                  </ul>
+              </li>
+              <li onClick={Reclas}>Reclassification
+                  <ul id="reclasssub" hidden>
+                        <li>Building Plan Application</li>
+                        <li>Building Plan Approval</li>
+                  </ul>
+              </li>
+            </ul>
+          </div>
           <div id="inside-left">
           <button id="t1" onClick={toptab1}>Applicant Details</button>
           <button id="t2" onClick={toptab2}>Application Details</button>
